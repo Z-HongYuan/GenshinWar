7f5ed3eadf9c11a02170231b621b915e68cb6a7e
Z-HongYuan <1939218189@qq.com>
2025-02-23 01:51:51 +0800

    更新项目 重命名文件,移动文件,添加AIController

---
+++ Source/GenshinWar/Public/Gameplay/GameMode/GenshinGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GenshinGameMode.generated.h"
+
+/**
+ * 基础游戏规则
+ */
+UCLASS()
+class GENSHINWAR_API AGenshinGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
